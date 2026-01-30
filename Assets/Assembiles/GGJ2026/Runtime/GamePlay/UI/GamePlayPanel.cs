b9839839135f3e55621b7ad87449f25ca56d311f
NicoIer <NicoIer@163.com>
2026-01-30 20:03:52 +0800

    Add Addressables config and initial scenes
    Added Unity Addressables configuration files and data, including asset groups and data builders. Introduced initial scenes (GameEntry, GameHome, GamePlay) and supporting meta files. Updated EditorBuildSettings and added SceneTemplateSettings for project setup.

--- Assets/Assembiles/GGJ2026/Runtime/GamePlay/UI/GamePlayPanel.cs
+++ Assets/Assembiles/GGJ2026/Runtime/GamePlay/UI/GamePlayPanel.cs
@@ -7,16 +7,12 @@ namespace Jump.GamePlay
 {
     public class GamePlayPanel : UIPanel
     {
-        [SerializeField] private Button backHomeButton;
-
         private void Awake()
         {
-            backHomeButton.onClick.AddListener(OnBackHomeButtonClicked);
         }
 
         private void OnBackHomeButtonClicked()
         {
-            Global.gameFlow.stateMachine.Change<HomeState>();
         }
     }
 }
